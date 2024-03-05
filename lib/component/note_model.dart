57b1fcaebc336651761b4ec67f511aed9a98362e
Atchara  Pathon <156067380+pkacr@users.noreply.github.com>
2024-03-05 15:25:04 +0700

    ToDoList Application with Firebase

---
+++ lib/component/note_model.dart
@@ -0,0 +1,9 @@
+class Note {//โครงสร้างข้อมูลที่จะเก็บ ไว้เก็บโน๊ต
+  String id;
+  String subtitle;
+  String title;
+  String time;
+  int image;
+  bool isDon;
+  Note(this.id, this.subtitle, this.time, this.image, this.title, this.isDon);
+}
